f8b481987280578631e046c7e5d01261b15d9cfb
aleferoiz <aleferoizdev@gmail.com>
2026-09-23 22:54:13 -0300

    Configure client to connect to online server

--- client/config.js
+++ client/config.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Depois de publicar o servidor na Render, cole aqui a URL recebida.
-// Exemplo: "https://coutec-server.onrender.com"
 window.COUTEC_CONFIG = {
-  onlineServerUrl: "",
+  onlineServerUrl: "https://coutec.onrender.com",
 };
